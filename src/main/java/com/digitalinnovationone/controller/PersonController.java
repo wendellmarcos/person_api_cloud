ee65e87950e234e34ed84379b6f01030730e13cc
Wendell Marcos <wendell.marcos.code@gmail.com>
2023-09-08 03:16:10 +0100

    first commit

---
+++ src/main/java/com/digitalinnovationone/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.digitalinnovationone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @RequestMapping
+    public String getBook(){
+        return "API Test!";
+    }
+}
