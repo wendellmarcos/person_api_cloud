aa30e41a4cbbfbf38fb983cc69fe4eb09b6ecf07
Wendell Marcos <wendell.marcos.code@gmail.com>
2023-09-08 03:36:51 +0100

    update Maven pomxml

--- src/main/java/com/digitalinnovationone/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/controller/PersonController.java
@@ -1,12 +1,12 @@
 package com.digitalinnovationone.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("api/v1/people")
 public class PersonController {
-
     @RequestMapping
     public String getBook(){
         return "API Test!";
